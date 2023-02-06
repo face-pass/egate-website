b7e5a67237efd94d4ab34fd5f6e2aad1f7fcfe6e
kazuki13 <kazuki.mk.13@gmail.com>
2023-02-07 00:24:48 +0900

    data

---
+++ src/js/heder.js
@@ -0,0 +1,55 @@
+
+import '../scc/heder.css';
+
+
+
+function App() {
+  return (
+<div >
+    <header class="header">
+      <div class="header__inner">
+          <h1 class="header__title header-title">
+            <a href="#">
+              E-gate
+            </a>
+          </h1>
+      </div>
+
+      <div class="hamburger-menu">
+          
+          <input type="checkbox" id="menu-btn-check"></input>
+          <label for="menu-btn-check" class="menu-btn"><span></span></label>
+          {/* ここからメニュー */}
+          <div class="menu-content">
+            <ul>
+            <li>
+                <a href="/">ホーム</a>
+              </li>
+              <li>
+                <a href="/2">登録</a>
+              </li>
+              <li>
+                <a href="/3">出席</a>
+              </li>
+              <li>
+                <a href="/4">出席一覧</a>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </header>
+    <main>
+      <div class="main">
+        main
+      </div>
+    </main>
+    <footer class="footer">
+      <div>
+        フッターエリア
+      </div>
+    </footer>
+  </div>
+  );
+}
+
+export default App;
